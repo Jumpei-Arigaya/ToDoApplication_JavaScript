2c8b051cd0d9a68e21fe051118391c8d57410538
jun <junpeeei0407@gmail.com>
2022-07-08 23:24:28 +0900

    DirectryName Changed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const form = document.getElementById("form");
+const input = (document.getElementById("input"));
+
+form.addEventListener('submit', (event) => {
+    console.log(input.value);
+    event.preventDefault();
+    input.value = "";
+});
