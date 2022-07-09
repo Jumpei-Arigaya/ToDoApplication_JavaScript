3b08715db2837039525ecf15047401e0949e3c61
jun <junpeeei0407@gmail.com>
2022-07-09 15:06:08 +0900

    afternoon

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const todo = input.innerText;
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(todo);
-    // document.createElement(todo);
-    // // ul.appendChild(li);
-    //aaakousinn
-
+    add();
     input.value = "";
 });
+
+function add() {
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    input.value = "";
+}
